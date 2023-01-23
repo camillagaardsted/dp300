a9cd0ab412257ff3243cf48387ffd3be5b4cea46
camillagaardsted <35761367+camillagaardsted@users.noreply.github.com>
2023-01-23 12:15:42 +0100

    added

---
+++ Lab1VM/azuread_users_and_groups.ps1
@@ -0,0 +1,61 @@
+﻿
+Connect-AzAccount
+
+$pn=Get-AzDomain |select -expandproperty domains
+
+$password="Pa55w.rd"
+$passwordSecure=ConvertTo-SecureString -AsPlainText $password -Force
+
+
+$username="otto"
+$Name="Otto Pilfinger"
+$upn = "$username@$pn"
+$mailnickname="ottoregnskab"
+$otto=New-AzADUser -DisplayName $Name -UserPrincipalName $upn -Password $passwordSecure -MailNickname $mailnickname
+
+$username="ottoline"
+$Name="Ottoline Pilfinger"
+$upn = "$username@$pn"
+$mailnickname="ottolinemarketing"
+$ottoline=New-AzADUser -DisplayName $Name -UserPrincipalName $upn -Password $passwordSecure -MailNickname $mailnickname
+
+
+
+$groupName="BI group"
+$mailnickname="bigruppen"
+$bigroup=New-AzADGroup -DisplayName $groupName -MailNickname $mailnickname 
+
+Add-AzADGroupMember -TargetGroupDisplayName $groupName -MemberUserPrincipalName $otto.UserPrincipalName,$ottoline.UserPrincipalName
+
+
+##################### IT department ##########################
+
+# Opret to IT folk i Azure AD
+$username="ivan"
+$Name="Ivan IT"
+$upn = "$username@$pn"
+$mailnickname="ivanit"
+$ivan=New-AzADUser -DisplayName $Name -UserPrincipalName $upn -Password $passwordSecure -MailNickname $mailnickname
+
+$username="frode"
+$Name="Frode Pilfinger"
+$upn = "$username@$pn"
+$mailnickname="frodeit"
+$frode=New-AzADUser -DisplayName $Name -UserPrincipalName $upn -Password $passwordSecure -MailNickname $mailnickname
+
+# Opret sikkerhedsgruppe til vores to IT folk
+$groupName="IT group"
+$mailnickname="itgruppen"
+$bigroup=New-AzADGroup -DisplayName $groupName -MailNickname $mailnickname 
+
+Add-AzADGroupMember -TargetGroupDisplayName $groupName -MemberUserPrincipalName $ivan.UserPrincipalName,$frode.UserPrincipalName
+
+
+# Opret sikkerhedsgruppe til vores to IT folk
+$groupName="DBA group"
+$mailnickname="dbagruppen"
+$dbagroup=New-AzADGroup -DisplayName $groupName -MailNickname $mailnickname 
+
+Add-AzADGroupMember -TargetGroupDisplayName $groupName -MemberUserPrincipalName $upn
+
+$upn | clip
